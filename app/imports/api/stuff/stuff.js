bfb2e0999617dada4c6f863c4f6227bb27bad33d
Jala Alamin <jala.alamin@wsu.edu>
2018-11-26 13:43:17 -0800

    Edited Add Recipe fields on add_stuff_page

--- app/imports/api/stuff/stuff.js
+++ app/imports/api/stuff/stuff.js
@@ -59,7 +59,62 @@ export const StuffSchema = new SimpleSchema({
       placeholder: 'john.doe@gmail.com',
     },
   },
+  recipe: {
+    label: 'Recipe Title',
+    type: String,
+    optional: false,
+    max: 50,
+    autoform: {
+      group: 'Stuff',
+      placeholder: 'Chocolate Chip Cookies',
+    },
+  },
+  ingredients: {
+    label: 'Ingredients',
+    type: String,
+    optional: false,
+    max: 1000,
+    autoform: {
+      group: 'Stuff',
+      placeholder:
+        '1 cup butter, softened\n' +
+        '1 cup white sugar\n' +
+        '1 cup packed brown sugar\n' +
+        '2 eggs\n' +
+        '2 teaspoons vanilla extract\n' +
+        '1 teaspoon baking soda\n' +
+        ' \n' +
+        '2 teaspoons hot water\n' +
+        '1/2 teaspoon salt\n' +
+        '3 cups all-purpose flour\n' +
+        '2 cups semisweet chocolate chips\n' +
+        '1 cup chopped walnuts\n',
+    },
+  },
+  directions: {
+    label: 'Directions',
+    type: String,
+    optional: false,
+    max: 1000,
+    autoform: {
+      group: 'Stuff',
+      placeholder:
+        'Preheat oven to 350 degrees F (175 degrees C).\n' +
+        'Cream together the butter, white sugar, and brown sugar until smooth. Beat in the eggs one at a time, then stir in the vanilla. Dissolve baking soda in hot water. Add to batter along with salt. Stir in flour, chocolate chips, and nuts. Drop by large spoonfuls onto ungreased pans.\n' +
+        'Bake for about 10 minutes in the preheated oven, or until edges are nicely browned.',
+    },
+  },
+  cuisineType: {
+    label: 'Cuisine Type',
+    type: String,
+    optional: false,
+    max: 50,
+    autoform: {
+      group: 'Stuff',
+      placeholder: 'Dessert',
+    },
+  },
 });
 
 Stuff.attachSchema(StuffSchema);
-console.log("hello")
+console.log('hello');
