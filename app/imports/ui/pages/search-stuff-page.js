8b6e9ee6b922ef99b4a37291b8156537df47bc09
Christian Svinth <christian.svinth@wsu.edu>
2018-11-30 12:57:54 -0800

    Got Variable input working

--- app/imports/ui/pages/search-stuff-page.js
+++ app/imports/ui/pages/search-stuff-page.js
@@ -1,18 +1,22 @@
+/* eslint-env jquery */
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { ReactiveVar } from 'meteor/reactive-var';
 import { Stuff } from '../../api/stuff/stuff.js';
 
 Template.Search_Stuff_Page.onCreated(function () {
   this.templateDictionary = new ReactiveDict();
   this.templateDictionary.set('searchValue', null);
+  this.searchParam = new ReactiveVar();
 });
 
 if (Meteor.isClient) {
   Template.Search_Stuff_Page.events({
-    'submit #search': function (event) {
+    'submit .center.aligned.form-wrapper-2.cf': function (event, instance) {
+      console.log('submit triggered');
+      instance.searchParam.set($('#searchValue').val());
       event.preventDefault();
-      Template.Search_Stuff_Page.templateDictionary.set('searchValue', ('searchValueI').val());
     },
   });
 }
@@ -23,6 +27,8 @@ Template.Search_Stuff_Page.helpers({
    * @returns {*} All of the Stuff documents.
    */
   stuffSearch() {
-    return Stuff.find();
+    const instance = Template.instance();
+    console.log(instance.searchParam);
+    return Stuff.find({ recipe: 'Eggs' });
   },
 });
