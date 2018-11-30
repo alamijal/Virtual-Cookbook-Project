1d4f71fab1245f778dce2c42d244e2446da05538
Christian Svinth <christian.svinth@wsu.edu>
2018-11-30 11:30:07 -0800

    Added Reactive-Dict to project; Started basic search

--- app/imports/ui/pages/search-stuff-page.js
+++ app/imports/ui/pages/search-stuff-page.js
@@ -1,12 +1,28 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Stuff } from '../../api/stuff/stuff.js';
+
+Template.Search_Stuff_Page.onCreated(function () {
+  this.templateDictionary = new ReactiveDict();
+  this.templateDictionary.set('searchValue', null);
+});
+
+if (Meteor.isClient) {
+  Template.Search_Stuff_Page.events({
+    'submit #search': function (event) {
+      event.preventDefault();
+      Template.Search_Stuff_Page.templateDictionary.set('searchValue', ('searchValueI').val());
+    },
+  });
+}
 
 Template.Search_Stuff_Page.helpers({
 
   /**
    * @returns {*} All of the Stuff documents.
    */
-  stuffList() {
+  stuffSearch() {
     return Stuff.find();
   },
 });
