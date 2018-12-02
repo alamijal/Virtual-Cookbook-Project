a4b461aee7490e41cfd97c4291680503569d1f16
Jala Alamin <jala.alamin@wsu.edu>
2018-12-01 21:10:02 -0800

    added filter recipe functionality by cuisineType

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -8,6 +8,33 @@ FlowRouter.route('/', {
   },
 });
 
+FlowRouter.route('/breakfastresults', {
+  name: 'List_Searched_Stuff_Page_Breakfast',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Searched_Stuff_Page_Breakfast' });
+  },
+});
+
+FlowRouter.route('/lunchresults', {
+  name: 'List_Searched_Stuff_Page_Lunch',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Searched_Stuff_Page_Lunch' });
+  },
+});
+
+FlowRouter.route('/dinnerresults', {
+  name: 'List_Searched_Stuff_Page_Dinner',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Searched_Stuff_Page_Dinner' });
+  },
+});
+
+FlowRouter.route('/dessertresults', {
+  name: 'List_Searched_Stuff_Page_Dessert',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'List_Searched_Stuff_Page_Dessert' });
+  },
+});
 FlowRouter.route('/list', {
   name: 'List_Stuff_Page',
   action() {
@@ -41,14 +68,6 @@ FlowRouter.route('/stuff/:_id', {
     BlazeLayout.render('App_Body', { main: 'View__Full_Recipe' });
   },
 });
-
-FlowRouter.route('/listSearched/:_id', {
-  name: 'List_Searched_Stuff_Page',
-  action() {
-    BlazeLayout.render('App_Body', { main: 'List_Searched_Stuff_Page' });
-  },
-});
-
 
 FlowRouter.notFound = {
   action() {
--- app/imports/ui/pages/list-searched-stuff-page.js
+++ app/imports/ui/pages/list-searched-stuff-page.js
@@ -1,14 +1,40 @@
 import { Template } from 'meteor/templating';
 import { Stuff } from '../../api/stuff/stuff.js';
 
+Template.List_Searched_Stuff_Page_Breakfast.helpers({
 
-Template.List_Searched_Stuff_Page.helpers({
+  /**
+   * @returns {*} All of the Stuff documents.
+   */
+  stuffList() {
+    return Stuff.find({ cuisineType: 'Breakfast' });
+  },
+});
+Template.List_Searched_Stuff_Page_Lunch.helpers({
 
   /**
    * @returns {*} All of the Stuff documents.
    */
   stuffList() {
     return Stuff.find({ cuisineType: 'Lunch' });
+  },
+});
+Template.List_Searched_Stuff_Page_Dinner.helpers({
+
+  /**
+   * @returns {*} All of the Stuff documents.
+   */
+  stuffList() {
+    return Stuff.find({ cuisineType: 'Dinner' });
+  },
+});
+Template.List_Searched_Stuff_Page_Dessert.helpers({
+
+  /**
+   * @returns {*} All of the Stuff documents.
+   */
+  stuffList() {
+    return Stuff.find({ cuisineType: 'Dessert' });
   },
 });
 
